b2b6213840680d364217aca9d354e9c829fbdd6d
Natalie Macias <nramirez787@gmail.com>
2016-10-01 09:13:43 -1000

    adding data to parse and grid set up

--- public/js/app.js
+++ public/js/app.js
@@ -1,47 +1,73 @@
-const CHART = document.getElementById("myChart");
-console.log('CHART', CHART);
+const CG = document.getElementById("commanding general");
+const COS = document.getElementById("cheif of staff");
+const DCGAR = document.getElementById("dcg army reserve");
 
-let myChart = new Chart(CHART, {
-    data: {datasets: [{
-            data: [
-                11,
-                16,
-                7,
-                3,
-                14,
-                6,
-                20
-            ],
-            backgroundColor: [
-                "#FF6384",
-                "#4BC0C0",
-                "#FFCE56",
-                "#E7E9ED",
-                "#36A2EB",
-                "#FFB05C",
-                "#CCDE44"
-            ]
-        }],
-        labels: [
-            "Nato",
-            "Allies",
-            "Army",
-            "Officials",
-            "Supporters",
-            "Joint",
-            "Staff"
-        ]
-      },
-    type: "polarArea",
-    options: {
-        elements: {
-            arc: {
-                borderColor: "#000000"
-            }
+// function getData() {
+//   $.getJSON({
+//     url: '/data/CG.json'
+//   }).done(function(data) {
+
+//     console.log(data);
+//   });
+// };
+// getData();
+
+// function parseData(data){
+//   console.log(data, 'parsedata data');
+//   var items = data.items;
+
+//   var events = items.map(function(cur) {
+//     var startDate = moment(cur.start.dateTime);
+//     var endDate = moment(cur.end.dateTime);
+//     var duration = moment.duration(endDate.diff(startDate)).hours();
+
+//     return {
+//       eventType: cur.summary,
+//       duration: duration
+//     }
+//   });
+//  }
+// parseData(getData());
+
+function createCharts (element, data, text){
+  let myChart = new Chart(element, {
+      data: {datasets: [{
+              data: data,
+              backgroundColor: [
+                  "#2f6cce",
+                  "#23b23b",
+                  "#037015",
+                  "#efec1c",
+                  "#a32d0d",
+                  "#690da3",
+                  "#707070"
+              ]
+          }],
+          labels: [
+              "Nato",
+              "Allies",
+              "Army",
+              "Officials",
+              "Supporters",
+              "Joint",
+              "Staff"
+          ]
         },
-        title: {
-            display: true,
-            text: 'General'
-        }
-    }
-});
+      type: "polarArea",
+      options: {
+          elements: {
+              arc: {
+                  borderColor: "#000000"
+              }
+          },
+          title: {
+              display: true,
+              text: text
+          }
+      }
+  });
+};
+
+createCharts(CG, [6, 8, 10, 11, 13, 16, 7], 'Commanding General');
+createCharts(COS, [6, 8, 10, 11, 13, 16, 7], 'Cheif of Staff');
+createCharts(DCGAR, [6, 8, 10, 11, 13, 16, 7], 'DCG Army Reserve');
