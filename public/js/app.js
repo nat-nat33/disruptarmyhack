4ee7fab68a53e230695a4393e1124058d2a2c3ac
Natalie Macias <nramirez787@gmail.com>
2016-10-01 12:32:52 -1000

    adding parsed data

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,37 @@
 const CG = document.getElementById("commanding general");
 const COS = document.getElementById("cheif of staff");
 const DCGAR = document.getElementById("dcg army reserve");
+const DCGNG = document.getElementById("dcg national guard");
+const DCGN = document.getElementById("dcg national");
+const DCGS = document.getElementById("dcg south");
 
-// function getData() {
-//   $.getJSON({
-//     url: '/data/CG.json'
-//   }).done(function(data) {
+function getData() {
+  $.getJSON({
+    url: '/data/CG.json'
+  }).done(function(data) {
+    var items = data.items;
+    var events = items.map(function(cur) {
+      var startDate = moment(cur.start.dateTime);
+      var endDate = moment(cur.end.dateTime);
+      var duration = moment.duration(endDate.diff(startDate)).hours();
 
-//     console.log(data);
-//   });
-// };
-// getData();
-
-// function parseData(data){
-//   console.log(data, 'parsedata data');
-//   var items = data.items;
-
-//   var events = items.map(function(cur) {
-//     var startDate = moment(cur.start.dateTime);
-//     var endDate = moment(cur.end.dateTime);
-//     var duration = moment.duration(endDate.diff(startDate)).hours();
-
-//     return {
-//       eventType: cur.summary,
-//       duration: duration
-//     }
-//   });
-//  }
-// parseData(getData());
+      return {
+        eventType: cur.summary,
+        duration: duration
+      }
+    });
+    var poop = events.reduce(function(previous, current){
+      if(!previous[current.eventType]){
+        previous[current.eventType] = 0;
+      }
+      previous[current.eventType] += current.duration;
+      return previous;
+    }, {});
+    // createCharts(CG, data, 'Commanding General');
+    console.log(poop, 'poop');
+  });
+};
+getData();
 
 function createCharts (element, data, text){
   let myChart = new Chart(element, {
@@ -57,17 +61,21 @@ function createCharts (element, data, text){
       options: {
           elements: {
               arc: {
-                  borderColor: "#000000"
+                  borderColor: "#efefef"
               }
           },
           title: {
               display: true,
-              text: text
+              text: text,
+              fontSize: 14
           }
       }
   });
 };
 
-createCharts(CG, [6, 8, 10, 11, 13, 16, 7], 'Commanding General');
-createCharts(COS, [6, 8, 10, 11, 13, 16, 7], 'Cheif of Staff');
-createCharts(DCGAR, [6, 8, 10, 11, 13, 16, 7], 'DCG Army Reserve');
+// createCharts(CG, [6, 8, 10, 11, 13, 16, 7], 'Commanding General');
+// createCharts(COS, [6, 8, 10, 11, 13, 16, 7], 'Cheif of Staff');
+// createCharts(DCGAR, [6, 8, 10, 11, 13, 16, 7], 'DCG Army Reserve');
+// createCharts(DCGNG, [6, 8, 10, 11, 13, 16, 7], 'DCG National Guard');
+// createCharts(DCGN, [6, 8, 10, 11, 13, 16, 7], 'DCG National');
+// createCharts(DCGS, [6, 8, 10, 11, 13, 16, 7], 'DCG South');
